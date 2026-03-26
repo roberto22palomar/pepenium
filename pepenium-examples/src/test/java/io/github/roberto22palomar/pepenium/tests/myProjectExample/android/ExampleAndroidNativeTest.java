fe885f4531ffefaa25229c23cc44589034441094
Rober Palomar <roberto22palomar@gmail.com>
2026-03-26 09:12:15 +0100

    feat: add toolkit assertions and example usage

--- pepenium-examples/src/test/java/io/github/roberto22palomar/pepenium/tests/myProjectExample/android/ExampleAndroidNativeTest.java
+++ pepenium-examples/src/test/java/io/github/roberto22palomar/pepenium/tests/myProjectExample/android/ExampleAndroidNativeTest.java
@@ -3,16 +3,21 @@ package io.github.roberto22palomar.pepenium.tests.myProjectExample.android;
 import io.github.roberto22palomar.pepenium.core.execution.TestTarget;
 import io.github.roberto22palomar.pepenium.core.runtime.BaseTest;
 import io.github.roberto22palomar.pepenium.toolkit.actions.ActionsApp;
+import io.github.roberto22palomar.pepenium.toolkit.assertions.AssertionsApp;
 import io.github.roberto22palomar.pepenium.toolkit.examples.myProjectExample.android.flows.ExampleNavigationFlowAndroid;
 import io.github.roberto22palomar.pepenium.toolkit.examples.myProjectExample.android.pages.BottomNavigationPage;
 import io.github.roberto22palomar.pepenium.toolkit.examples.myProjectExample.android.pages.SearchPage;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 @Slf4j
 @Tag("pepenium-example")
 public class ExampleAndroidNativeTest extends BaseTest {
+
+    private static final By SEARCH_QUICK_TAB = By.xpath("//*[@resource-id='btn-search-tab-search']");
+    private static final By HOME_TAB = By.xpath("//*[@resource-id='click_bottom_home']");
 
     @Override
     protected TestTarget getTarget() {
@@ -22,10 +27,13 @@ public class ExampleAndroidNativeTest extends BaseTest {
     @Test
     void basicNavigationFlow_shouldRun() {
         ActionsApp actionsApp = new ActionsApp(appiumDriver());
+        AssertionsApp assertionsApp = new AssertionsApp(appiumDriver());
         SearchPage searchPage = new SearchPage(actionsApp);
         BottomNavigationPage bottomNavigationPage = new BottomNavigationPage(actionsApp);
         ExampleNavigationFlowAndroid flow = new ExampleNavigationFlowAndroid(bottomNavigationPage, searchPage);
+        assertionsApp.assertVisible(SEARCH_QUICK_TAB);
         flow.runBasicNavigationFlow();
+        assertionsApp.assertVisible(HOME_TAB);
         log.info("Example Android native flow finished");
     }
 }
--- pepenium-examples/src/test/java/io/github/roberto22palomar/pepenium/tests/myProjectExample/ios/ExampleIOSNativeTest.java
+++ pepenium-examples/src/test/java/io/github/roberto22palomar/pepenium/tests/myProjectExample/ios/ExampleIOSNativeTest.java
@@ -3,16 +3,22 @@ package io.github.roberto22palomar.pepenium.tests.myProjectExample.ios;
 import io.github.roberto22palomar.pepenium.core.execution.TestTarget;
 import io.github.roberto22palomar.pepenium.core.runtime.BaseTest;
 import io.github.roberto22palomar.pepenium.toolkit.actions.ActionsAppIOS;
+import io.github.roberto22palomar.pepenium.toolkit.assertions.AssertionsAppIOS;
 import io.github.roberto22palomar.pepenium.toolkit.examples.myProjectExample.ios.flows.ExampleNavigationFlowIOS;
 import io.github.roberto22palomar.pepenium.toolkit.examples.myProjectExample.ios.pages.BottomNavigationPageIOS;
 import io.github.roberto22palomar.pepenium.toolkit.examples.myProjectExample.ios.pages.SearchPageIOS;
+import io.appium.java_client.AppiumBy;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 @Slf4j
 @Tag("pepenium-example")
 public class ExampleIOSNativeTest extends BaseTest {
+
+    private static final By SEARCH_QUICK_TAB = AppiumBy.accessibilityId("btn-search-tab-search");
+    private static final By HOME_TAB = AppiumBy.accessibilityId("click_bottom_home");
 
     @Override
     protected TestTarget getTarget() {
@@ -27,10 +33,13 @@ public class ExampleIOSNativeTest extends BaseTest {
     @Test
     void basicNavigationFlow_shouldRun() {
         ActionsAppIOS actionsApp = new ActionsAppIOS(appiumDriver());
+        AssertionsAppIOS assertionsApp = new AssertionsAppIOS(appiumDriver());
         SearchPageIOS searchPage = new SearchPageIOS(actionsApp);
         BottomNavigationPageIOS bottomNavigationPage = new BottomNavigationPageIOS(actionsApp);
         ExampleNavigationFlowIOS flow = new ExampleNavigationFlowIOS(bottomNavigationPage, searchPage);
+        assertionsApp.assertVisible(SEARCH_QUICK_TAB);
         flow.runBasicNavigationFlow();
+        assertionsApp.assertVisible(HOME_TAB);
         log.info("Example iOS native flow finished");
     }
 }
